4bd3d66e5224221e5b056d59bc51f1587bb028c7
Zacharia Njoroge <zacharianjoroge@Zacharias-MacBook-Pro-2.local>
2019-04-20 09:51:25 +0300

    Add setup files

---
+++ src/test/java/com/Oloola/Oloola/OloolaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Oloola.Oloola;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OloolaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
