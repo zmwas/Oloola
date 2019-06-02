ce93ce761ee84c9d13d6b755c71d0b7f3e481b64
Zacharia Njoroge <zacharianjoroge@Zacharias-MacBook-Pro-2.local>
2019-06-02 14:10:17 +0300

    Fix query

--- src/main/java/com/Oloola/Oloola/repository/TripRepository.java
+++ src/main/java/com/Oloola/Oloola/repository/TripRepository.java
@@ -13,7 +13,7 @@ public interface TripRepository extends JpaRepository<Trip, Long> {
     @Query(nativeQuery = true,
             value = "SELECT * FROM trip as t " +
                     "INNER JOIN location as l ON l.id = t.trip_start_id " +
-                    "AND ST_DWithin(ST_MakePoint(:latitude,:longitude)::geography, ST_MakePoint(l.latitude, l.longitude)::geography, 15000) " +
+                    "AND ST_DWithin(ST_MakePoint(:latitude,:longitude)::geography, ST_MakePoint(l.latitude, l.longitude)::geography, 10000) " +
                     "AND t.is_booked=false " +
                     "ORDER BY ST_Distance(ST_MakePoint(:latitude,:longitude)::geography, ST_MakePoint(l.latitude, l.longitude)::geography)")
     Optional<List<Trip>> filterTrips(Long collectionPointId, Long dropOffPointId, Double latitude, Double longitude);
