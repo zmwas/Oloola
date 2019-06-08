1b40cba50f1e62a3cbfa0b945754777ec3eb7203
Zacharia Njoroge <zacharianjoroge@Zacharias-MacBook-Pro-2.local>
2019-06-08 15:23:01 +0300

    Minor changes

--- src/main/java/com/Oloola/Oloola/repository/TripRepository.java
+++ src/main/java/com/Oloola/Oloola/repository/TripRepository.java
@@ -14,8 +14,8 @@ import java.util.Optional;
 public interface TripRepository extends JpaRepository<Trip, Long> {
     @Query(nativeQuery = true,
             value = "SELECT * FROM trip as t " +
-                    "INNER JOIN location as l ON l.id = t.trip_start_id " +
-                    "INNER JOIN location as k ON k.id = t.trip_destination_id " +
+                    "LEFT JOIN location as l ON l.id = t.trip_start_id " +
+                    "LEFT JOIN location as k ON k.id = t.trip_destination_id " +
                     "WHERE ST_DWithin(cast(ST_MakePoint(l.latitude,l.longitude) AS geography), cast(ST_MakePoint(:latitude,:longitude)AS geography) ,10000) " +
                     "AND ST_DWithin(cast(ST_MakePoint(k.latitude,k.longitude) AS geography), cast(ST_MakePoint(:latitude,:longitude)AS geography) ,15000) " +
                     "ORDER BY ST_Distance(cast(ST_MakePoint(l.latitude,l.longitude)AS geography), cast(ST_MakePoint(:dropLat,:dropLon)AS geography))")
